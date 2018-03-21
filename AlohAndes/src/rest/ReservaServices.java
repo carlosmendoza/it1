03c69e54b5b0357d11c68de6b9fd80163dcaa034
Carlos Mendoza <ca.mendoza@uniandes.edu.co>
2018-03-21 01:21:37 -0500

    mas cambios c:

--- AlohAndes/src/rest/ReservaServices.java
+++ AlohAndes/src/rest/ReservaServices.java
@@ -1,5 +1,94 @@
 package rest;
 
+import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import tm.TM;
+import vos.Oferta;
+import vos.Reserva;
+
+@Path("reservas")
 public class ReservaServices {
+	
+	@Context
+	private ServletContext context;
+
+	
+	private String getPath() {
+		return context.getRealPath("WEB-INF/ConnectionData");
+	}
+
+	private String doErrorMessage(Exception e) {
+		return "{ \"ERROR\": \"" + e.getMessage() + "\"}";
+	}
+	
+	
+
+	@GET
+	@Produces({ MediaType.APPLICATION_JSON })
+	public Response getReservas() {
+		
+		TM tm = new TM(getPath());
+		List<Reserva> reservas;
+		try {
+			reservas = tm.getAllReservas();
+		} catch (Exception e) {
+			return Response.status(500).entity(doErrorMessage(e)).build();
+		}
+		return Response.status(200).entity(reservas).build();
+	}
+	
+	@GET
+	@Path("idReserva/{id}")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public Response getOfertaPorId(@PathParam("id") String idReserva) {
+
+		TM tm = new TM(getPath());
+		Reserva o;
+		try {
+			if (idReserva == null )
+				throw new Exception("Id de la reserva no valido");
+			else
+			{
+			o = tm.darReservaPorId(Integer.parseInt(idReserva));
+			}
+		} catch (Exception e) {
+			return Response.status(500).entity(doErrorMessage(e)).build();
+		}
+		return Response.status(200).entity(o).build();
+	}
+	
+	
+	
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	
+	public Response addReserva(Reserva reserva) {
+		TM tm = new TM(getPath());
+		try {
+			tm.addReserva(reserva);
+		} catch (Exception e) {
+			return Response.status(500).entity(doErrorMessage(e)).build();
+		}
+		return Response.status(200).entity(reserva).build();
+	}
+	
+	
+	
+	
+	
+	
 
 }
